be2962cfc745101851598201c574a7e69924ef40
sss1rang1roman <roman.fedotov2011@gmail.com>
2025-11-04 12:58:12 +0300

    Add_bot.py

--- M1L3/bot.py
+++ M1L3/bot.py
@@ -21,23 +21,23 @@ def ban_user(message):
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
 
 @bot.message_handler(func=lambda message: True)
 def check_links(message):
-   
     if message.text and ('http://' in message.text.lower() or 'https://' in message.text.lower() or 't.me/' in message.text.lower()):
         chat_id = message.chat.id
         user_id = message.from_user.id
         user_status = bot.get_chat_member(chat_id, user_id).status
         
-       
         if user_status not in ['administrator', 'creator']:
             try:
-              
                 bot.ban_chat_member(chat_id, user_id)
-             
                 bot.delete_message(chat_id, message.message_id)
-                
                 bot.send_message(chat_id, f"Пользователь @{message.from_user.username} был забанен за отправку ссылки.")
             except Exception as e:
                 print(f"Ошибка при бане пользователя: {e}")
