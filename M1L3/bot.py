97623c7e6a7e51f72a1877a4e3e058172bfba8c7
sss1rang1roman <roman.fedotov2011@gmail.com>
2025-11-04 13:01:00 +0300

    add_Новый пользователь

--- M1L3/bot.py
+++ M1L3/bot.py
@@ -23,7 +23,7 @@ def ban_user(message):
 
 @bot.message_handler(content_types=['new_chat_members'])
 def make_some(message):
-    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.send_message(message.chat.id, 'Новый пользователь')
     bot.approve_chat_join_request(message.chat.id, message.from_user.id)
 
 
